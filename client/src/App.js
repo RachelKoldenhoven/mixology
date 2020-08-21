7925b108f977d54911c6d6b0b5706b60cf9cbac1
RachelKoldenhoven <rachel@koldenfrozen.com>
2020-08-20 21:04:33 -0600

    Add react-md

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,13 @@
 import React, { Component } from 'react';
+import { Button } from '@react-md/button';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardActions,
+  CardContent,
+} from '@react-md/card';
+import { Text } from '@react-md/typography';
 import './App.css';
 
 class App extends Component {
@@ -12,12 +21,26 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <h1>Drinks</h1>
-        {this.state.drinks.map((drink) => (
-          <div key={drink.id}>{drink.name}</div>
-        ))}
-      </div>
+      <>
+        <div className="App">
+          <h1>Drinks</h1>
+          {this.state.drinks.map((drink) => (
+            <div key={drink.id}>{drink.name}</div>
+          ))}
+        </div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Text>Here is some text to display in the card. It is </Text>
+          </CardContent>
+          <CardActions>
+            <Button>Action 1</Button>
+            <Button>Action 2</Button>
+          </CardActions>
+        </Card>
+      </>
     );
   }
 }
